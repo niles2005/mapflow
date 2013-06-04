f15be2a309fc4830f9e26559518c719c1c504f52
niles <niles2005@gmail.com>
2013-06-04 15:15:23 +0800

    'datefilter'

--- js/controllers.js
+++ js/controllers.js
@@ -3,95 +3,106 @@
 /* Controllers */
 
 var projectList = [
-    {
-        "id": "p1", 
-        "name": "shanghai", 
-        "dataVendor": "OSM", 
-		"imageUrl":"img/osm.png",
-        "createTime": "2013-5-29 15:03:02", 
-        "creator": "niles", 
-        "info": "Test map project for shanghai"
-    }, 
-    {
-        "id": "p2", 
-        "name": "test", 
-        "dataVendor": "OSM", 
-		"imageUrl":"img/osm.png",
-        "createTime": "2013-5-28 10:33:27", 
-        "creator": "niles", 
-        "info": "Test map project"
-    },
-    {
-        "id": "p3", 
-        "name": "njcc", 
-        "dataVendor": "OSM", 
-		"imageUrl":"img/osm.png",
-        "createTime": "2013-5-29 13:13:43", 
-        "creator": "niles", 
-        "info": "map project for njcc"
-    }
+	{
+		"id": "p1",
+		"name": "shanghai",
+		"dataVendor": "OSM",
+		"imageUrl": "img/osm.png",
+		"createTime": "2013-5-29 15:03:02",
+		"creator": "niles",
+		"info": "Test map project for shanghai"
+	},
+	{
+		"id": "p2",
+		"name": "test",
+		"dataVendor": "OSM",
+		"imageUrl": "img/osm.png",
+		"createTime": "2013-5-28 10:33:27",
+		"creator": "niles",
+		"info": "Test map project"
+	},
+	{
+		"id": "p3",
+		"name": "njcc",
+		"dataVendor": "OSM",
+		"imageUrl": "img/osm.png",
+		"createTime": "2013-5-29 13:13:43",
+		"creator": "niles",
+		"info": "map project for njcc"
+	}
 ];
 
 var projectDetail = {
-	"id": "p1", 
-	"name": "shanghai", 
-	"dataVendor": "OSM", 
-	"imageUrl":"img/osm.png",
-	"createTime": "2013-5-29 15:03:02", 
-	"creator": "niles", 
+	"id": "p1",
+	"name": "shanghai",
+	"dataVendor": "OSM",
+	"imageUrl": "img/osm.png",
+	"createTime": "2013-5-29 15:03:02",
+	"creator": "niles",
 	"info": "Test map project for shanghai",
-	"files":[
-		{"name":"bj_park_pat_region.xmd","time":"2013-5-29 13:2:43","size":24235},
-		{"name":"bj_railway_pat_polyline.xmd","time":"2013-5-29 3:13:43","size":54386},
-		{"name":"destOcean.xmd","time":"2013-5-29 13:13:4","size":426787568},
-		{"name":"green_region.xmd","time":"2013-5-9 13:13:43","size":57845}
+	"files": [
+		{"name": "bj_park_pat_region.xmd", "time": "2013-5-29 13:2:43", "size": 24235},
+		{"name": "bj_railway_pat_polyline.xmd", "time": "2013-5-29 3:13:43", "size": 54386},
+		{"name": "destOcean.xmd", "time": "2013-5-29 13:13:4", "size": 426787568},
+		{"name": "green_region.xmd", "time": "2013-5-9 13:13:43", "size": 57845}
 	]
 };
 
 angular.module('myApp.controllers', []).
-  controller('ProjectsCtrl', function($rootScope) {
-        $rootScope.projects = projectList;
-  }).	
-  controller('Project1Ctrl', function($scope,$routeParams) {
-        $scope.project = projectDetail;
-  }).	
-  controller('MyCtrl1', [function() {
+		controller('ProjectsCtrl', function($rootScope) {
+	$rootScope.projects = projectList;
+}).
+		controller('Project1Ctrl', function($scope, $routeParams) {
+	$scope.project = projectDetail;
+}).
+		controller('MyCtrl1', [function() {
 
-  }])
-  .controller('ProjectNew', function($rootScope,$scope,$location) {
-        $scope.project =  {
-            "id": "",
-            "name": "",
-            "dataVendor": "",
-            "imageUrl":"img/osm.png",
-            "createTime": "",
-            "creator": "default",
-            "info": ""
-        }
+	}])
+		.controller('ProjectNew', function($rootScope, $scope, $location,$filter) {
+	$scope.project = {
+		"id": "",
+		"name": "",
+		"dataVendor": "",
+		"imageUrl": "img/osm.png",
+		"createTime": "",
+		"creator": "default",
+		"info": ""
+	}
 
-        $scope.save = function() {
-            $scope.project.id =  'p' + ($rootScope.projects.length + 1);
-            $scope.project.createTime = new Date();
+	$scope.save = function() {
+		$scope.project.id = 'p' + ($rootScope.projects.length + 1);
+		$scope.project.createTime = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
 
-            $rootScope.projects.push($scope.project);
-            $location.path('#/projects');
-        };
+		$rootScope.projects.push($scope.project);
+		$location.path('#/projects');
+	};
 
 
-  }).controller('ProjectEdit', function($rootScope,$scope,$location,$routeParams) {
-        var res =   $rootScope.projects.filter(function(value){
-            return value.id == $routeParams.projectId;
-        });
-        if(res.length == 1){
-            $scope.project =  res[0];
-        }
+}).controller('ProjectEdit', function($rootScope, $scope, $location, $routeParams) {
+	var res = $rootScope.projects.filter(function(value) {
+		return value.id == $routeParams.projectId;
+	});
+	if (res.length == 1) {
+		$scope.project = res[0];
+	}
 
-//        console.dir($scope.project);
+	$scope.save = function() {
+		$scope.project.createTime = new Date();
+		$location.path('#/projects');
 
-        $scope.save = function() {
-            $scope.project.createTime = new Date();
-            $location.path('#/projects');
+	};
+}).controller(CompilingCtrl);
 
-        };
-    }).controller('CompilingCtrl', function($scope,$routeParams) {
-  });
+
+function CompilingCtrl($scope) {
+	var mycanvas = document.getElementById("myCanvasTag");
+	var mycontext = mycanvas.getContext('2d');
+	mycontext.fillStyle = 'rgb(0,0,255)';
+	mycontext.fillRect(0, 0, 400, 400);
+	mycontext.fillStyle = 'rgb(255,0,0)';
+	mycontext.fillRect(50, 50, 300, 300);
+	mycontext.fillStyle = 'rgb(0,255,0)';
+	mycontext.fillRect(100, 100, 200, 200);
+	mycontext.fillStyle = 'rgb(100,100,100)';
+	mycontext.fillRect(125, 175, 150, 25);
+}
