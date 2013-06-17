dbe920aaed9af704651a417ea63b3c62819f25c0
niles <niles2005@gmail.com>
2013-06-17 11:00:04 +0800

    'update'

--- js/CompileContentCtrl.js
+++ js/CompileContentCtrl.js
@@ -1,3 +1,35 @@
 function CompileContentCtrl($scope) {
     $scope.test = "aab";
+    $scope.props = [
+        {'exist':0,"showPixel":0.8,margin:1},
+        {'exist':0},
+        {'exist':0},
+        {'exist':0},
+        {'exist':0},
+        
+        {'exist':1},
+        {'exist':1},
+        {'exist':0},
+        {'exist':0},
+        {'exist':0},
+        
+        {'exist':0},
+        {'exist':0},
+        {'exist':1},
+        {'exist':1},
+        {'exist':0},
+        
+        {'exist':0},
+        {'exist':0},
+        {'exist':0},
+        {'exist':0},
+        {'exist':0}];
+    $scope.setProp = function(prop) {
+        if($scope.myVar) {
+            $scope.myVar='';
+        } else {
+            $scope.myVar='my-class';
+        }
+        $scope.currProp = prop;
+    };
 }
