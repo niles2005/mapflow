4f87094b3f0cc31b18e0c846776bb2198ca52344
niles <niles2005@gmail.com>
2013-07-05 17:18:23 +0800

    update

--- js/app.js
+++ js/app.js
@@ -21,5 +21,5 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
 
 
 
-    $routeProvider.otherwise({redirectTo: '/projects'});
+    $routeProvider.otherwise({redirectTo: '/files'});
   }]);
