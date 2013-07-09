1b40975e99ab04dcfa801d3d2ba0ef95442e55fe
zoboya <rayzang8@gmail.com>
2013-07-09 15:40:01 +0800

    updata

--- js/FilesCtrl.js
+++ js/FilesCtrl.js
@@ -1,15 +1,89 @@
 'use strict';
 
-var files = [
-	{"name": "bj_park_pat_region.xmd", "time": "2013-5-29 13:2:43", "size": 24235},
-	{"name": "bj_railway_pat_polyline.xmd", "time": "2013-5-29 3:13:43", "size": 54386},
-	{"name": "destOcean.xmd", "time": "2013-5-29 13:13:4", "size": 426787568},
-	{"name": "green_region.xmd", "time": "2013-5-9 13:13:43", "size": 57845}
-]
+function FilesCtrl($scope, $filter) {
+    filesList();
+    $('.dashboard-tabs a').removeClass('selected');
+    $('.dashboard-tabs a[href="#/files"]').addClass('selected');
 
-function FilesCtrl($scope) {
-	$scope.files = files;
-	$('.dashboard-tabs a').removeClass('selected');
-	$('.dashboard-tabs a[href="#/files"]').addClass('selected');
+    $scope.setUploadFiles = function (filePath) {
+        $scope.tempUploadFiles = [];
+        for (var i in  $('#fileToUpload')[0].files) {
+            var tempFile = $('#fileToUpload')[0].files[i];
+            if (tempFile instanceof File) {
+                tempFile["time"] = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
+                $scope.tempUploadFiles.push(tempFile);
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+            }
+        }
+    }
+
+    $scope.uploadFile = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        var xhr = new XMLHttpRequest();
+        xhr.enctype = 'multipart/form-data';
+        xhr.upload.addEventListener("progress", uploadProgress, false);
+        xhr.addEventListener("load", $scope.uploadComplete, false);
+        xhr.addEventListener("error", uploadFailed, false);
+        xhr.addEventListener("abort", uploadCanceled, false);
+        xhr.open("POST", "file?project=shanghai&action=upload");
+        var fd = new FormData();
+        fd.append("fileToUpload", $('#fileToUpload')[0].files[0]);
+        xhr.send(fd);
+    }
+
+
+    $scope.uploadComplete = function (evt) {
+        /* This event is raised whenthe server send back a response */
+        filesList();
+    }
+
+    function uploadProgress(evt) {
+        if (evt.lengthComputable) {
+            var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+            $('#progressPercent').attr('value', percentComplete);
+            $('#progressNumber').text(percentComplete + '%');
+        }
+    }
+
+    function uploadFailed(evt) {
+
+    }
+
+    function uploadCanceled(evt) {
+
+    }
+
+    function filesList() {
+        $.ajax({
+            url: "/mapflow/file?project=shanghai&action=list",
+            type: "POST",
+            dataType: "json"
+        }).done(function (data) {
+                if (data && data instanceof Array) {
+                    $scope.files = data;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                }
+
+            });
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
